6661bdaff5f2ae3df21a8b252a3b7ca875b04a13
Zahran Rafif <zahranlumia@gmail.com>
2022-04-13 16:26:25 +0700

    Update Activity 7

--- app/src/main/java/com/example/aplikasisqlite/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/aplikasisqlite/adapter/TemanAdapter.java
@@ -1,25 +1,34 @@
 package com.example.aplikasisqlite.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.aplikasisqlite.MainActivity;
 import com.example.aplikasisqlite.R;
+import com.example.aplikasisqlite.database.DBController;
 import com.example.aplikasisqlite.database.Teman;
+import com.example.aplikasisqlite.edit_teman;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> listData;
-
+    private Context control;
 
     public TemanAdapter(ArrayList<Teman> listData) {
         this.listData = listData;
@@ -27,23 +36,60 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
 
 
     @Override
-    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public TemanViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
         LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
         View view = layoutInf.inflate(R.layout.row_data_teman, parent,false);
+        control = parent.getContext();
         return new TemanViewHolder(view);
     }
 
 
     @Override
-    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
+    public void onBindViewHolder(TemanAdapter.TemanViewHolder holder, int position) {
         //deklarasi variabel
-        String nm,tlp;
+        String nm,tlp, id;
+        id = listData.get(position).getId();
         nm = listData.get(position).getNama();
         tlp = listData.get(position).getTelpon();
+        DBController db = new DBController(control);
+
         holder.namaTxt.setTextColor(Color.BLUE);
         holder.namaTxt.setTextSize(20);
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(control,holder.cardku);
+                popupMenu.inflate(R.menu.menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.mnEdit:
+                                Intent i = new Intent(control, edit_teman.class);
+                                i.putExtra("id",id);
+                                i.putExtra("nama",nm);
+                                i.putExtra("telpon", tlp);
+                                control.startActivity(i);
+                                break;
+                            case R.id.mnHapus:
+                                HashMap<String, String> values = new HashMap<>();
+                                values.put("id", id);
+                                db.DeleteData(values);
+                                Intent j = new Intent(control, MainActivity.class);
+                                control.startActivity(j);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                return false;
+
+            }
+        });
     }
 
 
